dfefabb6a5193b1867bcd5be6da7e889787587df
William Silva <williamreis.wsilva@gmail.com>
2023-07-14 14:08:13 -0300

    commit inicial: exercicio Typescript

---
+++ exercicio_ts.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiplicacao = (num1, num2) => num1 * num2;
+const saudacao = (nome) => `Olá ${nome}`;
+console.log(multiplicacao(5, 100));
+console.log(saudacao("William Silva"));
---
+++ exercicio_ts.ts
@@ -0,0 +1,5 @@
+const multiplicacao = (num1: number, num2:number): number => num1 * num2;
+const saudacao = (nome: string): string => `Olá ${nome}`;
+
+console.log(multiplicacao(5,100));
+console.log(saudacao("William Silva"));
